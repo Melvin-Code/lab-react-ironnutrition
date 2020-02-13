44d1e61050a17e97dd397675cd65c2a861450354
melvin-Code <melvingonzalez470@gmail.com>
2020-02-12 22:54:15 -0500

    first comit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css'
+import FoodBox from './FoodBox';
+import foods from './foods.json';
+
 
 class App extends Component {
+  state = {
+    foods,
+    foodForm: false
+  }
+  showFoods = () => {
+   let foodList = this.state.foods.map((eachFood, i) => {
+      return <FoodBox 
+      key={i} {...eachFood}
+    />
+    })
+    return foodList
+  }
+  toggleNewFoodForm = () => {
+    if(this.state.foodForm){
+      <form
+    }
+  }
   render() {
+  
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+      <button onClick={this.toggleNewFoodForm}>Add new food</button>
+      {this.showFoods()}
       </div>
     );
   }
